8b6127d1eacba380a60c6c6dfba409685fdc43c4
Dominic Montelongo <padma@email.com>
2020-04-08 09:35:19 -0700

    Add player consturctor idea to sketch out

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+//Back End
+
+player.prototype.roll
+
+player.prototype.hold
+
+player.prototype.restart
+
+
+
+
+// Front End
+$(Document).ready(function(){
+    event.preventDefault()
+
+
+})
+
+
+// function  Player(name) {
+    this.playerName = name;
+    this.currentId =[]
+    this.totalScore = 0;
+    this.turnScore = 0;
+}
+
