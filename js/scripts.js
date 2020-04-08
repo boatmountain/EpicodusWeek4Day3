5d611709f819b1aadeaf79b645c54ce2dd1c6fb9
Dominic Montelongo <padma@email.com>
2020-04-08 10:37:25 -0700

    Exploring possible code

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,28 @@
 //Back End
+function  Player(name) {
+  this.playerName = name;
+  this.currentId =[]
+  this.totalScore = 0;
+  this.turnScore = 0;
+}
 
-player.prototype.roll
 
-player.prototype.hold
+player.prototype.roll = function(){
+    // var dieRoll = Math.floor(Math.random()*6) +1;
+    if (dieRoll === 1){ 
+      //layer.currentID.turnscore = 0;
+    } else {
+      //player.currentID.turnScore += dieRoll;
+    }
+}
 
-player.prototype.restart
+player.prototype.hold = function(){
+
+}
+
+player.prototype.restart = function(){
+
+}
 
 
 
@@ -12,15 +30,12 @@ player.prototype.restart
 // Front End
 $(Document).ready(function(){
     event.preventDefault()
-
+var dieRoll = Math.floor(Math.random()*6) +1;
 
 })
 
 
-// function  Player(name) {
-    this.playerName = name;
-    this.currentId =[]
-    this.totalScore = 0;
-    this.turnScore = 0;
-}
+
+
+
 
