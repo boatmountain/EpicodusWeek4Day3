30df83ad46d9151960901f7fd581e5e5a9b334f7
Dominic Montelongo <padma@email.com>
2020-04-08 10:58:42 -0700

    Exploring possible code

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,23 @@ function  Player(name) {
   this.turnScore = 0;
 }
 
+function dieRoll(){
+ return Math.floor(Math.random()*6) +1;
+};
+
 
 player.prototype.roll = function(){
-    // var dieRoll = Math.floor(Math.random()*6) +1;
-    if (dieRoll === 1){ 
-      //layer.currentID.turnscore = 0;
-    } else {
-      //player.currentID.turnScore += dieRoll;
+    // var dieR(6)oll = Math.floor(Math.random()*6) +1;
+    if (pdieRoll === 1){ 
+      //player.currentID.turnscore = 0;
+    } else (pdieRoll =! 1){
+        return score + 
+
+      // ask player to hold or roll again      //player.currentID.turnScore += dieRoll;
     }
 }
 
-player.prototype.hold = function(){
+  //player.currentID.totalScore += turnScore;player.prototype.hold = function(){
 
 }
 
@@ -36,6 +42,7 @@ var dieRoll = Math.floor(Math.random()*6) +1;
 
 
 
+https://gist.github.com/damianesteban/09640735173f8a2893d4
 
 
 
